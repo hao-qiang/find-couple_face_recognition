8366a42d7668c720e1200944ad32ef5f07cfcc4b
Hao Qiang <wind209@sina.com>
2018-08-16 14:32:14 +0800

    Update find_couple_image.py

--- find_couple_image.py
+++ find_couple_image.py
@@ -6,7 +6,7 @@ import sys
 
 
 img_path = sys.argv[1]
-couple_dir = "./data/couple/"
+couple_dir = "./data/couple1/"
 single_dir = "./data/single/"
 
 # extract couple's facial features
@@ -43,88 +43,65 @@ if len(single_imgs_names) != 0:
         known_face_encodings.append(face_recognition.face_encodings(single_img)[0])
 
 
-process_this_frame = True
+frame = cv2.imread(img_path)
+rgb_frame = frame[:, :, ::-1]
 
+face_locations = face_recognition.face_locations(rgb_frame)
+face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-while True:
-    frame = cv2.imread(img_path)
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-    rgb_small_frame = small_frame[:, :, ::-1]
+face_names = []
+for face_encoding in face_encodings:
+    # See if the face is a match for the known face(s)
+    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+    name = "Unknown"
+    # If a match was found in known_face_encodings, just use the first one.
+    if True in matches:
+        first_match_index = matches.index(True)
+        name = known_face_names[first_match_index]
+    face_names.append(name)
 
-    # Only process every other frame of video to save time
-    if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-        face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
-
-            # If a match was found in known_face_encodings, just use the first one.
-            if True in matches:
-                first_match_index = matches.index(True)
-                name = known_face_names[first_match_index]
-
-            face_names.append(name)
-
-    process_this_frame = not process_this_frame
-
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
-        
-        if name == bf_name:
-            top_x_bf = (right + left) // 2
-            top_y_bf = top
-            
-        if name == gf_name:
-            top_x_gf = (right + left) // 2
-            top_y_gf = top
-            # make up
-            face_landmarks_list = face_recognition.face_landmarks(frame[top-10:bottom+10,left-10:right+10,:][:,:,::-1])
-            if face_landmarks_list!=[]:
-                triangle = np.asarray([face_landmarks_list[0]['left_eyebrow']], np.int32)+(left-10,top-10)
-                cv2.fillConvexPoly(frame, triangle, (39, 54, 68, 128), lineType=4)
-                triangle = np.asarray([face_landmarks_list[0]['right_eyebrow']], np.int32)+(left-10,top-10)
-                cv2.fillConvexPoly(frame, triangle, (39, 54, 68, 128), lineType=4)
-                triangle = np.asarray([face_landmarks_list[0]['top_lip']], np.int32)+(left-10,top-10)
-                cv2.fillConvexPoly(frame, triangle, (0, 0, 150, 128), lineType=4)
-                triangle = np.asarray([face_landmarks_list[0]['bottom_lip']], np.int32)+(left-10,top-10)
-                cv2.fillConvexPoly(frame, triangle, (0, 0, 150, 128), lineType=4)
-                triangle = np.asarray([face_landmarks_list[0]['left_eye']], np.int32)+(left-10,top-10)
-                cv2.polylines(frame, triangle, True, (0, 0, 0, 255), thickness=2, lineType=4)
-                triangle = np.asarray([face_landmarks_list[0]['right_eye']], np.int32)+(left-10,top-10)
-                cv2.polylines(frame, triangle, True, (0, 0, 0, 255), thickness=2, lineType=4)
-            
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+# Display the results
+for (top, right, bottom, left), name in zip(face_locations, face_names):
+    bottom += 30
     
-    if (bf_name in face_names) and (gf_name in face_names):
-        target_x = (top_x_bf + top_x_gf) // 2
-        target_y = min(top_y_bf, top_y_gf) - 50
-        cv2.line(frame,(top_x_bf,top_y_bf),(target_x, target_y),(0,0,255), 3)
-        cv2.line(frame,(top_x_gf,top_y_gf),(target_x, target_y),(0,0,255), 3)
-        cv2.ellipse(frame,(target_x-7, target_y),(30,20),60,0,360,(0,0,255),-1)
-        cv2.ellipse(frame,(target_x+7, target_y),(30,20),-60,0,360,(0,0,255),-1)
-        
-    # Display the resulting image
-    cv2.imshow('Image', frame)
+    if name == bf_name:
+        top_x_bf = (right + left) // 2
+        top_y_bf = top
+    if name == gf_name:
+        top_x_gf = (right + left) // 2
+        top_y_gf = top
+        # make up
+        face_landmarks_list = face_recognition.face_landmarks(frame[top-10:bottom+10,left-10:right+10,:][:,:,::-1])
+        if face_landmarks_list!=[]:
+            triangle = np.asarray([face_landmarks_list[0]['left_eyebrow']], np.int32)+(left-10,top-10)
+            cv2.fillConvexPoly(frame, triangle, (39, 54, 68, 128), lineType=4)
+            triangle = np.asarray([face_landmarks_list[0]['right_eyebrow']], np.int32)+(left-10,top-10)
+            cv2.fillConvexPoly(frame, triangle, (39, 54, 68, 128), lineType=4)
+            triangle = np.asarray([face_landmarks_list[0]['top_lip']], np.int32)+(left-10,top-10)
+            cv2.fillConvexPoly(frame, triangle, (0, 0, 150, 128), lineType=4)
+            triangle = np.asarray([face_landmarks_list[0]['bottom_lip']], np.int32)+(left-10,top-10)
+            cv2.fillConvexPoly(frame, triangle, (0, 0, 150, 128), lineType=4)
+            triangle = np.asarray([face_landmarks_list[0]['left_eye']], np.int32)+(left-10,top-10)
+            cv2.polylines(frame, triangle, True, (0, 0, 0, 255), thickness=2, lineType=4)
+            triangle = np.asarray([face_landmarks_list[0]['right_eye']], np.int32)+(left-10,top-10)
+            cv2.polylines(frame, triangle, True, (0, 0, 0, 255), thickness=2, lineType=4)
+            
+    # Draw a box around the face
+    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Draw a label with a name below the face
+    cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+    font = cv2.FONT_HERSHEY_DUPLEX
+    cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-# Release handle to the webcam
-cv2.destroyAllWindows()
+if (bf_name in face_names) and (gf_name in face_names):
+    target_x = (top_x_bf + top_x_gf) // 2
+    target_y = min(top_y_bf, top_y_gf) - 50
+    cv2.line(frame,(top_x_bf,top_y_bf),(target_x, target_y),(0,0,255), 3)
+    cv2.line(frame,(top_x_gf,top_y_gf),(target_x, target_y),(0,0,255), 3)
+    cv2.ellipse(frame,(target_x-7, target_y),(30,20),60,0,360,(0,0,255),-1)
+    cv2.ellipse(frame,(target_x+7, target_y),(30,20),-60,0,360,(0,0,255),-1)
+
+# Display the resulting image
+cv2.imshow('Image', frame)
+cv2.waitKey(-1)
